8fdbeb36695dee7449adc9d80b2dfe4659476d5d
Emilio Sala <emilio_k88@hotmail.com>
2026-03-17 12:03:35 +0100

    ADD: upload thumbnail, not in memory

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -10,11 +11,11 @@ import (
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
-	videoID, err := uuid.Parse(videoIDString)
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
-		return
-	}
+    videoID, err := uuid.Parse(videoIDString)
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Invalid video ID format", err)
+        return
+    }
 
 	token, err := auth.GetBearerToken(r.Header)
 	if err != nil {
@@ -28,10 +29,49 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	// 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	maxMemory := int64(10 << 20)
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error parsing multipart form", err)
+		return
+	}
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error getting thumbnail file", err)
+		return
+	}
+	defer file.Close()
+	mediaType := header.Header.Get("Content-Type")
+	fileData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Error reading thumbnail file", err)
+		return
+	}
 
-	// TODO: implement the upload here
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not the owner of this video", nil)
+		return
+	}
+	videoThumbnails[videoID] = thumbnail{
+		data:      fileData,
+		mediaType: mediaType,
+	}
+	thumbURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video.ThumbnailURL = &thumbURL
+	fmt.Println("Thumbnail URL:", thumbURL)
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
